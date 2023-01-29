17506abc9c6b03a004360bed9431d694a0ee5a61
Maddie Gareth <maddiega@pr-p5.kzn.21-school.ru>
2023-01-29 16:41:37 +0300

    Add semantic nav tag to allPages

--- src/pages/allPages/allPages.ts
+++ src/pages/allPages/allPages.ts
@@ -8,33 +8,35 @@ export class AllPagesPage extends Block {
     // language=hbs
     return `
     <main class="centered-wrapper all-pages">
-        <h2 class="all-pages__header">Project pages</h2>
+      <h2 class="all-pages__header">Project pages</h2>
+      <nav class="all-pages__nav">
         <ul class="all-pages__list">
-            <li>
-                <a href="./auth">Авторизация</a>
-            </li>
-            <li>
-                <a href="./reg">Регистрация</a>
-            </li>
-            <li>
-                <a href="./chat">Чат</a>
-            </li>
-            <li>
-                <a href="./profile">Профиль</a>
-            </li>
-            <li>
-                <a href="./edit-profile">Редактирование профиля</a>
-            </li>
-            <li>
-                <a href="./edit-password">Изменение пароля</a>
-            </li>
-            <li>
-                <a href="./404">404</a>
-            </li>
-            <li>
-                <a href="./5xx">5xx</a>
-            </li>
+          <li>
+            <a href="./auth">Авторизация</a>
+          </li>
+          <li>
+            <a href="./reg">Регистрация</a>
+          </li>
+          <li>
+            <a href="./chat">Чат</a>
+          </li>
+          <li>
+            <a href="./profile">Профиль</a>
+          </li>
+          <li>
+            <a href="./edit-profile">Редактирование профиля</a>
+          </li>
+          <li>
+            <a href="./edit-password">Изменение пароля</a>
+          </li>
+          <li>
+            <a href="./404">404</a>
+          </li>
+          <li>
+            <a href="./5xx">5xx</a>
+          </li>
         </ul>
+      </nav>
     </main>
     `
   }
