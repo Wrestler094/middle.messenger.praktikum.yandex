f2d3aecca9ac0d251fa7a956ae432c26009a5a58
Maddie Gareth <maddiega@ge-g6.kzn.21-school.ru>
2023-02-22 22:00:51 +0300

    Save tmp types

--- typings/app.d.ts
+++ typings/app.d.ts
@@ -3,6 +3,27 @@ declare global {
 
   export type Keys<T extends Record<string, unknown>> = keyof T
   export type Values<T extends Record<string, unknown>> = T[Keys<T>]
+
+  // TODO: chats
+  // TODO: active chat
+  // TODO: messages
+  export interface AppState {
+    appIsInited: boolean
+    isLoading: boolean
+    loginFormError: string | null
+    user: User | null
+  }
+
+  export interface User {
+    avatar: string
+    display_name: string
+    email: string
+    first_name: string
+    id: number
+    login: string
+    phone: string
+    second_name: string
+  }
 }
 
 export {}
