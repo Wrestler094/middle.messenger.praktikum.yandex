614201ab3021240e29c380ed39f001fbceff9975
Maddie Gareth <maddiega@ge-g6.kzn.21-school.ru>
2023-02-25 16:50:49 +0300

    Fix main window header

--- src/modules/chatMainWindow/__header/chatMainWindow__header.ts
+++ src/modules/chatMainWindow/__header/chatMainWindow__header.ts
@@ -1,10 +1,29 @@
-import Block from 'core/Block'
-import avatar from 'static/avatar.png'
+import { Block, Store } from 'core'
+import defaultAvatar from 'static/avatar.png'
 import call from 'static/call.png'
 import options from 'static/options.png'
 import './chatMainWindow__header.css'
 
-export class MainWindowHeader extends Block<Record<string, never>> {
+interface MainWindowHeaderProps {
+  avatar: string
+  name: string
+}
+
+export class MainWindowHeader extends Block<MainWindowHeaderProps> {
+  constructor () {
+    const store = Store.getState()
+    // @ts-expect-error
+    const currentChat = store.chats.find((chat) => chat.id === store.activeChatId)
+    const avatar = currentChat.avatar != null
+      ? 'https://ya-praktikum.tech/api/v2/resources' + String(currentChat.avatar)
+      : defaultAvatar
+
+    super({
+      avatar,
+      name: currentChat.title
+    })
+  }
+
   static componentName = 'MainWindowHeader'
 
   protected render (): string {
@@ -12,8 +31,8 @@ export class MainWindowHeader extends Block<Record<string, never>> {
     return `
       <header class="chat-header">
         <div class="chat-header__user">
-          <img class="chat-header__user-image" src="${avatar}" alt="Изображение собеседника">
-          <h3 class="chat-header__user-name">Florencio Dorrance</h3>
+          <img class="chat-header__user-image" src="{{avatar}}" alt="Изображение собеседника">
+          <h3 class="chat-header__user-name">{{name}}</h3>
           <p class="chat-header__user-online">Online</p>
         </div>
         <div class="chat-header__call">
