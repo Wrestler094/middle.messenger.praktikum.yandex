2e64a7e681d584553062175eb9f50743b2fa6862
Maddie Gareth <maddiega@va-c5.kzn.21-school.ru>
2023-03-08 15:20:37 +0300

    Add eslint-webpack-plugin and fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-const isProduction = process.env.NODE_ENV === "production";
-const stylesHandler = MiniCssExtractPlugin.loader;
+const isProduction = process.env.NODE_ENV === 'production'
+const stylesHandler = MiniCssExtractPlugin.loader
 
 const config = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     open: true,
-    host: "localhost",
+    host: 'localhost',
     historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html'
     }),
 
     new MiniCssExtractPlugin(),
+
+    new ESLintPlugin()
 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
@@ -31,42 +34,42 @@ const config = {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
+        loader: 'ts-loader',
+        exclude: ['/node_modules/']
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
+        use: [stylesHandler, 'css-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
+        type: 'asset'
+      }
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
     alias: {
-      "handlebars": "handlebars/dist/handlebars.js",
-      "api": path.resolve(__dirname, "./src/api"),
-      "core": path.resolve(__dirname, "./src/core"),
-      "components": path.resolve(__dirname, "./src/components"),
-      "helpers": path.resolve(__dirname, "./src/helpers"),
-      "pages": path.resolve(__dirname, "./src/pages"),
-      "services": path.resolve(__dirname, "./src/services"),
-      "static": path.resolve(__dirname, "./static")
+      handlebars: 'handlebars/dist/handlebars.js',
+      api: path.resolve(__dirname, './src/api'),
+      core: path.resolve(__dirname, './src/core'),
+      components: path.resolve(__dirname, './src/components'),
+      helpers: path.resolve(__dirname, './src/helpers'),
+      pages: path.resolve(__dirname, './src/pages'),
+      services: path.resolve(__dirname, './src/services'),
+      static: path.resolve(__dirname, './static')
     }
-  },
-};
+  }
+}
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production'
   } else {
-    config.mode = "development";
+    config.mode = 'development'
   }
-  return config;
-};
+  return config
+}
